7698c03b9cca0b71a31912a65335d28547a8136c
muratilgun <muratilgun34@gmail.com>
2022-03-09 20:12:44 +0300

    ReverseInPlace

--- Algorithms/10_Reverse_Array/Program.cs
+++ Algorithms/10_Reverse_Array/Program.cs
@@ -6,7 +6,12 @@ namespace _10_Reverse_Array
     {
         /*Ters çevirmek istediğim inputumu int array değişkenin içerisine atıyorum. -Satır 11
           gelen dizini for döngüsü ile dönüyorum satır 12
-          gelen arrayin 
+          reversed değişkenin i. indexine gelen input'un i eksi eksi birinci değerini atıyorum.
+          örn : i=0 reverserved[0] = input[6-0-1]  output reversed[0] = 6
+              : i=1 reverserved[1] = input[6-1-1]  output reversed[1] = 5 ...
+          şeklinde gelen arrayi tersine döndürebilirim.
+          Her hangibir içerik eklenmediğinden veya kaldırılmadığından bu algoritma yerindede yapılabilinir.
+          array içerisindeki öğeleri ters sıralı gönderebilirim. Bu algoritmada sadece yarısını tekrarlamak yeterli olacaktır.
           
           */
         static int[] Reverse(int[] input)
@@ -19,10 +24,23 @@ namespace _10_Reverse_Array
             }
             return reversed;
         }
+
+        static void ReverseInPlace(int[] input)
+        {
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                int temp = input[i];
+                input[i] = input[input.Length - i - 1];
+                input[input.Length - i - 1] = temp;
+            }
+        }
+
         static void Main(string[] args)
         {
             int[] arr = { 1, 2, 3, 4, 5, 6 };
-            Array.ForEach(Reverse(arr),Console.WriteLine);
+            //Array.ForEach(Reverse(arr),Console.WriteLine);
+            ReverseInPlace(arr);
+            Array.ForEach(arr,Console.WriteLine);
         }
     }
 }
