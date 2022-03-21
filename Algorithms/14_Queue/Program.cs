8077f85c0d494168aefc9cbabff191bbf2e412e4
muratilgun <muratilgun34@gmail.com>
2022-03-21 23:18:07 +0300

    Queue

---
+++ Algorithms/14_Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace _14_Queue
+//First in First out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(8);
+            queue.Enqueue(20);
+            queue.Enqueue(23);
+
+            int removedItem = queue.Dequeue(); //1
+            Console.WriteLine(removedItem);
+            Console.WriteLine(queue.Dequeue()); //8
+            Console.WriteLine(queue.Peek());
+            int current;
+            while (queue.TryDequeue(out current))
+            {
+                Console.WriteLine(current);
+            }
+        }
+    }
+}
