e13c01b9e81e221637d74213ea361818f098e1b1
muratilgun <muratilgun34@gmail.com>
2022-03-22 20:20:49 +0300

    Stack

---
+++ Algorithms/16_Stack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _16_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            Console.WriteLine("Start Main");
+            stack.Push("Main");
+            Console.WriteLine("Start ResponseBuilder");
+            stack.Push("ResponseBuilder");
+            Console.WriteLine("Start CallExternalService");
+            stack.Push("CallExternalService");
+            Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("Start ParseExternalData");
+            stack.Push("ParseExternalData");
+            Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("END " + stack.Pop());
+            Console.WriteLine("END " + stack.Pop());
+
+
+
+        }
+    }
+}
