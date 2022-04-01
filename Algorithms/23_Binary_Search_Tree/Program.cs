af78ccb81c3f55bf905e6bfdd923b587e5734e4c
muratilgun <muratilgun34@gmail.com>
2022-04-01 11:58:10 +0300

    BinarySearchTree

---
+++ Algorithms/23_Binary_Search_Tree/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _23_Binary_Search_Tree
+{
+    class Program
+    {
+        class Node
+        {
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public int Data { get; set; }
+
+        }
+
+        class BinarySearchTree
+        {
+            public static Node Insert(Node root, int value)
+            {
+                if (root == null)
+                {
+                    root = new Node();
+                    root.Data = value;
+                    return root;
+                }
+                else
+                {
+                    if (value < root.Data) {
+                        root.Left = Insert(root.Left, value);
+                    }
+                    else if (value > root.Data) {
+                        root.Right = Insert(root.Right, value);
+                    }
+                }
+                return root;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Node rootNode = new Node();
+            rootNode.Data = 4;
+            BinarySearchTree.Insert(rootNode, 2);
+            BinarySearchTree.Insert(rootNode, 3);
+            BinarySearchTree.Insert(rootNode, 5);
+            BinarySearchTree.Insert(rootNode, 6);
+            BinarySearchTree.Insert(rootNode, 4);
+        }
+    }
+}
